b3974d0eb0cb8775d6ef9113a652a0e5c87dcb41
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-27 08:05:43 +0530

    Project Initialization

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import "./index.css";
+
+const Header = () => (
+    <>
+        <nav className="nav-header">
+            <div className="nav-content">
+                <p className="website-logo">RA</p>
+                <ul className="nav-menu">
+                    <li>
+                        <img 
+                        className="social-network-img"
+                        src="https://assets.ccbp.in/frontend/react-js/projects-linkedin-img.png" 
+                        alt="linked-in" />
+                    </li>
+                    <li>
+                        <img 
+                        className="social-network-img"
+                        src="https://assets.ccbp.in/frontend/react-js/projects-github-img.png" 
+                        alt="Git-Hub" />
+                    </li>
+                    <li>
+                        <img 
+                        className="social-network-img"
+                        src="https://assets.ccbp.in/frontend/react-js/projects-twitter-img.png" 
+                        alt="Twitter" />
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>
+)
+
+export default Header
